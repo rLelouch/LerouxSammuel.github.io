7cdd52f337ab499adc531d75285b40e6f050d7f9
rLelouch <108684388+rLelouch@users.noreply.github.com>
2022-07-24 04:12:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const txtAnim = document.querySelector('#bonjour');
+
+new Typewriter (txtAnim, {
+    loop: true
+})
+.changeDelay(500)
+
+.typeString('<span style="color:blue">Bon</span><span style="color:white;text-shadow:1px 1px 2px white, 0 0 1em white, 0 0 0.2em blue">jo</span><span style="color:red">ur,</span>')
+.pauseFor(100)
+.deleteChars(8)
+
+.typeString('<span style="color:white;text-shadow:1px 1px 2px white, 0 0 1em white, 0 0 0.2em blue">He</span><span style="color:red">y,</span>')
+.pauseFor(100)
+.deleteChars(4)
+
+.typeString('<span style="color:black">Gut</span><span style="color:red">en <span><span style="color:yellow">Tag,</span>')
+.pauseFor(100)
+.deleteChars(10)
+
+.typeString('<span style="color:red">N<span style="color:yellow">ĭ H</span><span style="color:red">ăo,</span>')
+.pauseFor(100)
+
+.start()
